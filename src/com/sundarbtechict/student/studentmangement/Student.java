d96b25317905180d232336fc350fd716a5ae9701
sundar~ <sundar~@Sundar>
2017-04-02 23:05:50 +0530

    student management corrections

---
+++ src/com/sundarbtechict/student/studentmangement/Student.java
@@ -0,0 +1,21 @@
+package com.sundarbtechict.student.studentmangement;
+
+public class Student {
+		int regno;
+		String name;
+		String dob;
+		String email;
+		String mobile;
+		String dept;
+		Student(int r,String n,String db,String e,String m,String dt)
+		{
+			regno=r;
+			name=n;
+			dob=db;
+			email=e;
+			mobile=m;
+			dept=dt;
+		}
+
+
+}
---
+++ src/com/sundarbtechict/student/studentmangement/StudentManagementMain.java
@@ -0,0 +1,55 @@
+package com.sundarbtechict.student.studentmangement;
+import java.util.*;
+
+public class StudentManagementMain {
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		Crud c= new Crud();
+		int n=1;
+		do{
+			System.out.println("--------------------------");
+			System.out.println("	choices:");
+			System.out.println("1.	create");
+			System.out.println("2.	Read");
+			System.out.println("3.	Read all");
+			System.out.println("4.	Update");
+			System.out.println("5.	Delete");
+			System.out.println("6.	Delete All");
+			System.out.println("0.	Exit");
+			System.out.println("--------------------------");
+			System.out.println("Enter your choice:");
+			n=s.nextInt();
+			if(n==1)
+			{
+				c.create();
+			}
+			else if(n==2)
+			{
+				c.read();
+			}
+			else if(n==3)
+			{
+				c.readAll();
+			}
+			else if(n==4)
+			{
+				c.update();
+			}
+			else if(n==5)
+			{
+				c.delete();
+			}
+			else if(n==6)
+			{
+				c.deleteAll();
+			}
+			else
+			{
+				System.out.println("Ivalid chioce");
+			}
+		}while(n!=0);
+
+	}
+
+}
